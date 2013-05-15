381592942ed3e965dac57b6a66e9246b7e305177
itsrag@gmail.com <itsrag@gmail.com>
2013-05-15 11:06:25 +0530

    renaming script

--- files/rename.sh
+++ files/rename.sh
@@ -1,14 +1,13 @@
 mkdir shortened
-var=$( ls flip* )
+var=$( ls pointinfos/flip* )
 echo $var
 k=1
 for j in $var
 do
-split -b 8000 $j splitted
-    for i in $( ls splitted* )
+split -b 8000 $j pointinfos/splitted
+    for i in $( ls pointinfos/splitted* )
     do
     mv -f $i shortened/trip_$k.txt
     ((k++))
     done
-rm $j
 done
